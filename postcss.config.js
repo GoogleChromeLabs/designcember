6d80ebfb08de53cb6553e3591239d794b5d8467e
Adam Argyle <argyle@google.com>
2021-10-18 15:09:29 -0700

    adds range and custom media

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 const postcssJitProps = require('postcss-jit-props')
 const postcssNesting = require('postcss-nesting')
+const postcssCustomMedia = require('postcss-custom-media')
+const postcssMediaRanges = require('postcss-media-minmax')
 const postcssImport = require('postcss-import')
 const postcssCQFill = require('cqfill/postcss')
 
@@ -8,6 +10,8 @@ const OpenProps = require('@argyleink/open-props')
 module.exports = {
   plugins: [
     postcssImport({ path: ['src'] }),
+    postcssCustomMedia(),
+    postcssMediaRanges(),
     postcssJitProps(OpenProps),
     postcssNesting(),
     postcssCQFill(),
