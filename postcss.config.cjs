4640451fd56ce53c956449ca7f2131ab264b1aaa
Adam Argyle <argyle@google.com>
2021-10-22 09:22:36 -0700

    fixes custom media not being processed in prod builds

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -10,7 +10,21 @@ const OpenProps = require('@argyleink/open-props')
 module.exports = {
   plugins: [
     postcssImport({ path: ['src'] }),
-    postcssCustomMedia(),
+    postcssCustomMedia({
+      importFrom: [
+        {
+          customMedia: {
+            '--sm': '(width >= 720px)',
+            '--md': '(width >= 1024px)',
+            '--lg': '(width >= 1680px)',
+            '--xl': '(width >= 1920px)',
+            '--motionOK': '(prefers-reduced-motion: no-preference)',
+            '--dark': '(prefers-color-scheme: dark)',
+            '--light': '(prefers-color-scheme: light)',
+          }
+        }
+      ]
+    }),
     postcssMediaRanges(),
     postcssJitProps(OpenProps),
     postcssNesting(),
