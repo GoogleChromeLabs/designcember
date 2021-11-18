c518a88e476ec58192ed7467cd9456adaccdcf6a
Adam Argyle <argyle@google.com>
2021-11-18 09:13:20 -0800

    update to latest open props

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,19 +3,7 @@ const postcssNesting = require('postcss-nesting')
 const postcssCustomMedia = require('postcss-custom-media')
 const postcssMediaRanges = require('postcss-media-minmax')
 const postcssImport = require('postcss-import')
-
 const OpenProps = require('@argyleink/open-props')
-const flatten = data => Object.assign({}, 
-  ...function _flatten(o) { 
-    return [].concat(...Object.keys(o)
-      .map(k => 
-        typeof o[k] === 'object' ?
-          _flatten(o[k]) : 
-          ({[k]: o[k]})
-      )
-    );
-  }(data)
-)
 
 module.exports = {
   plugins: [
@@ -35,7 +23,7 @@ module.exports = {
       ]
     }),
     postcssMediaRanges(),
-    postcssJitProps(flatten(OpenProps)),
+    postcssJitProps(OpenProps),
     postcssNesting(),
   ],
 }
